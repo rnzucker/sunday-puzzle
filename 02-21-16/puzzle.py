6d9286979b2d644269acd66e509481450b9e2a91
[Rick Zucker] <[rnz@uw.edu]>
2016-02-29 20:07:53 -0800

    Program works. Finds "stranger", "stringer", and "stronger". Code is messy, and needs more documentation. That comes tomorrow.

--- 02-21-16/puzzle.py
+++ 02-21-16/puzzle.py
@@ -35,16 +35,54 @@ def main():
                 temp = words[i][0:8].rstrip()
                 # Append word to end
                 eight_words.append(temp)
-                print(eight_words[j], eight_words[j][4:7])
+                print(eight_words[j])
                 j = j + 1
     print("\n", j, "eight letter words with a G\n")
 
-
-    for i in range(len(eight_words)-2):
+    # Go through list of eight G words. If three words have the same first three letters,
+    # create a new list of all words with those same first three letters. Starting from zero
+    # up to n-2, search through remaining words for matches of fifth through eighth letters.
+    # If next one in list doesn't match, move to next one in the outermost loop of this common
+    # three letter prefix
+    num_eight_gs = len(eight_words)
+    i = 0
+    while i < (num_eight_gs-2):
         if (eight_words[i][0:3] == eight_words[i+1][0:3]) and (eight_words[i][0:3] == eight_words[i+2][0:3]):
-            print(eight_words[i], eight_words[i+1], eight_words[i+2])
-            if (eight_words[i][4:7] == eight_words[i+1][4:7]) and (eight_words[i][4:7] == eight_words[i+2][4:7]):
-                print(eight_words[i], eight_words[i+1], eight_words[i+2])
+            # print(eight_words[i], eight_words[i+1], eight_words[i+2])
+            three_same = []
+            three_same.append(eight_words[i])
+            three_same.append(eight_words[i+1])
+            three_same.append(eight_words[i+2])
+            j = 0
+            # Loop through until you hit a different three starting letters. Start three beyond the
+            # current value of i. Hence the i+3+j calculation
+            while (i+3+j) < num_eight_gs and (eight_words[i][0:3] == eight_words[i+3+j][0:3]):
+                three_same.append(eight_words[i+3+j])
+                j += 1
+            print("Three or more same", three_same)
+
+            # three_same is a list of three or more words where the first three letters are the same.
+            # Compare letters five to eight in the first word to the same letters in all the remaining
+            # words. If you don't get three matches, then start with the second word and do this again.
+            k = 0
+            while k < (len(three_same)-2):
+                m = 1
+                end_match = 1
+                rear_match = []
+                rear_match.append(three_same[k])
+                while k+m < len(three_same):
+                    if three_same[k][4:8] == three_same[k+m][4:8]:
+                        end_match += 1
+                        rear_match.append(three_same[k+m])
+                        # print("End match", end_match, rear_match)
+                        if end_match >= 3:
+                            print("REAR MATCH", rear_match)
+                    m += 1
+                k += 1
+            i = i + 2 + j
+            # if (eight_words[i][4:7] == eight_words[i+1][4:7]) and (eight_words[i][4:7] == eight_words[i+2][4:7]):
+            #     print(eight_words[i], eight_words[i+1], eight_words[i+2])
+        i += 1
 
 
 
