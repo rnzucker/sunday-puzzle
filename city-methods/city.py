11803398ca8c00d69f4600fceca3141f3a93ad93
[Rick Zucker] <[rnz@uw.edu]>
2016-03-23 12:15:43 -0700

    Switch to using properties, and added alphabetized list of letters. Dealt with spaces in names. Still need more comments. Will eventually use getter and setter to control for bad values in country/state field.

--- city-methods/city.py
+++ city-methods/city.py
@@ -6,33 +6,41 @@
 
 class City:
     VOWELS = "aeiou"
-    CONSONANTS = "bcdghjklmnpqrstvwxyz"
+    CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
     def __init__(self, name=None, city_type=None, state_name=None, country_name=None):
-        self.name = name
-        self.city_type = city_type
-        self.state_name = state_name
+        self.name         = name
+        self.city_type    = city_type
+        self.state_name   = state_name
         self.country_name = country_name
         # print("City fields:", self.name, self.city_type, self.state_name, self.country_name)
 
+    @property
     def num_vowels(self):
         count = 0
-
         for i in range(len(self.name)):
             if self.name.lower()[i] in City.VOWELS:
                 count += 1
         return count
 
+    @property
     def num_consonants(self):
         count = 0
-
         for i in range(len(self.name)):
             if self.name.lower()[i] in City.CONSONANTS:
                 count += 1
         return count
 
-    def city_name(self):
-        return self.name
+    @property
+    def num_letters(self):
+        # Return the number of letters in the city name excluding spaces
+        return len(self.name.replace(" ", ""))
+
+    @property
+    def alphabetized_letters(self):
+        # Return the name with the letters alphabetized
+        alpha_city = "".join(sorted(self.name.lower()))
+        return alpha_city.replace(" ", "")
 
 
 def main():
@@ -52,8 +60,9 @@ def main():
     print(len(city_list), "cities")
 
     for i in range(len(city_list)):
-        print("City {0} has {1} vowels and {2} consonants".format(city_list[i].city_name(), city_list[i].num_vowels(), \
-                                                                  city_list[i].num_consonants()))
+        print("{0} ({4}) has {1} letters, {2} vowels and {3} consonants".format(city_list[i].name, city_list[i].num_letters,
+                                                             city_list[i].num_vowels, city_list[i].num_consonants,\
+                                                             city_list[i].alphabetized_letters))
 
 
 # Check for interactive session
