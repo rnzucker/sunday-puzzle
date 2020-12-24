eab7dc02a64297ce8d0f447ac1d8ce60828bed8c
Rick Zucker <rzucker4@comcast.net>
2020-12-24 12:22:55 -0800

    Added comment back

--- 19-09-29/puzzle.py
+++ 19-09-29/puzzle.py
@@ -35,6 +35,7 @@ def main():
                 first_e = words[i].find('e')
                 if (words[i].find('e', first_e+1) != -1):
                     # Know that there is at least one character after the first 'e' due to the newline
+                    # Know that there are at least two e's. There could be more than two though. Should be fixed.
                     if ((words[i].find('i') == words[i].rfind('i')) and (words[i].find('o') == words[i].rfind('o')) and
                         (words[i].find('u') == words[i].rfind('u'))):
                         # Making sure there is only one i, o, and u
